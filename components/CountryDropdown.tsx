85fe01d21b4779f554c822fba5697b162dfa0c1c
donchi1 <mikkyonuma@gmail.com>
2023-09-20 07:15:02 +0100

    country dropdown component fixed

--- components/CountryDropdown.tsx
+++ components/CountryDropdown.tsx
@@ -71,7 +71,7 @@ function CountryDropdown({ state, setState }: {state: object, setState: React.Di
       </option>
       <option value="Cook Islands">Cook Islands</option>
       <option value="Costa Rica">Costa Rica</option>
-      <option value="Cote D'ivoire">Cote D'ivoire</option>
+      <option value="Cote D'ivoire">Cote D`&apos;`ivoire</option>
       <option value="Croatia">Croatia</option>
       <option value="Cuba">Cuba</option>
       <option value="Cyprus">Cyprus</option>
@@ -144,13 +144,13 @@ function CountryDropdown({ state, setState }: {state: object, setState: React.Di
       <option value="Kenya">Kenya</option>
       <option value="Kiribati">Kiribati</option>
       <option value="Korea, Democratic People's Republic of">
-        Korea, Democratic People's Republic of
+        Korea, Democratic People`&apos;`s Republic of
       </option>
       <option value="Korea, Republic of">Korea, Republic of</option>
       <option value="Kuwait">Kuwait</option>
       <option value="Kyrgyzstan">Kyrgyzstan</option>
       <option value="Lao People's Democratic Republic">
-        Lao People's Democratic Republic
+        Lao People`&apos;`s Democratic Republic
       </option>
       <option value="Latvia">Latvia</option>
       <option value="Lebanon">Lebanon</option>
